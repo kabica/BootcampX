182348e9dcaf1b3a5bd7fa8db5ca1380a77addb1
Kevin A. Bica <kalex.bica@gmail.com>
2020-02-03 20:26:29 +0000

    tried alternate workflow starting at cohorts, instead of stduents

--- 2_queries_joins/cohort_assignment_duration.sql
+++ 2_queries_joins/cohort_assignment_duration.sql
@@ -1,6 +1,3 @@
 
-SELECT sum(assignment_submissions.duration) as total_duration
-FROM assignment_submissions
-JOIN students ON students.id = student_id
-JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name = 'FEB12';
+SELECT day, COUNT(assignments) as total_assignments
+FROM assignments;
